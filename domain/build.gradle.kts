49f7d7979397fd2dfbc6ee98fa5cd9b47422b040
Filippo <filippo.nardini@studio.unibo.it>
2021-12-17 19:38:08 +0100

    Remove dependency on serialization

--- domain/build.gradle.kts
+++ domain/build.gradle.kts
@@ -9,6 +9,4 @@ dependencies {
     implementation("it.unibo.tuprolog", "theory", "0.20.4")
     implementation("it.unibo.tuprolog", "parser-core", "0.20.4")
     implementation("it.unibo.tuprolog", "parser-theory", "0.20.4")
-    implementation("it.unibo.tuprolog", "serialize-core", "0.20.4")
-    implementation("it.unibo.tuprolog", "serialize-theory", "0.20.4")
 }

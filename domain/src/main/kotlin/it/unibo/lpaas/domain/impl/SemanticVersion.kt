a571ac2f429b310df093ef7a6c06396c8f1b87d2
Filippo <filippo.nardini@studio.unibo.it>
2021-12-17 19:09:19 +0100

    Minor change to SemanticVersion

--- domain/src/main/kotlin/it/unibo/lpaas/domain/impl/SemanticVersion.kt
+++ domain/src/main/kotlin/it/unibo/lpaas/domain/impl/SemanticVersion.kt
@@ -8,7 +8,7 @@ import it.unibo.lpaas.domain.Version
  * Just a simple representation inspired by the semantic versioning system.
  * It just supports a version expressed in the for `X.Y.Z`.
  */
-data class SemanticVersion private constructor (val x: Int, val y: Int, val z: Int) : Version {
+class SemanticVersion private constructor (val x: Int, val y: Int, val z: Int) : Version {
 
     override fun compareTo(other: Version): Int {
         if (other !is SemanticVersion) {
@@ -23,8 +23,28 @@ data class SemanticVersion private constructor (val x: Int, val y: Int, val z: I
         z.compareTo(other.z),
     ).firstOrNull { it != 0 } ?: 0
 
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+
+        other as SemanticVersion
+
+        if (x != other.x) return false
+        if (y != other.y) return false
+        if (z != other.z) return false
+
+        return true
+    }
+
+    override fun hashCode(): Int {
+        var result = x
+        result = 31 * result + y
+        result = 31 * result + z
+        return result
+    }
+
     companion object {
-        fun of(x: Int, y: Int, z: Int): Version? =
+        fun of(x: Int, y: Int, z: Int): SemanticVersion? =
             SemanticVersion(x, y, z).takeUnless { listOf(x, y, z).any { it < 0 } }
     }
 }

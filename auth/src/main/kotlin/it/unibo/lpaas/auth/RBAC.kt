576e32c2f9b574c828ec03948de709bf8340aa0e
Alberto Marfoglia <alberto.marfoglia@studio.unibo.it>
2021-12-23 12:21:28 +0100

    Implement default RBAC constructor

--- auth/src/main/kotlin/it/unibo/lpaas/auth/RBAC.kt
+++ auth/src/main/kotlin/it/unibo/lpaas/auth/RBAC.kt
@@ -1,5 +1,6 @@
 package it.unibo.lpaas.auth
 
+import it.unibo.lpaas.auth.impl.SimpleRBAC
 import it.unibo.lpaas.core.Tag
 
 interface RBAC {
@@ -8,4 +9,14 @@ interface RBAC {
     fun removePermission(role: Role, tag: Tag)
     fun addPermissions(role: Role, tags: List<Tag>)
     fun removePermissions(role: Role, tags: List<Tag>)
+
+    companion object {
+        fun default(): RBAC = SimpleRBAC().apply {
+            addPermissions(Role.CLIENT, listOf("getAllGoals", "getAllGoalsIndex", "getGoalByName").map { Tag(it) })
+            addPermissions(Role.CONFIGURATOR, listOf(
+                "getAllGoals", "getAllGoalsIndex", "getGoalByName", "createGoal", "replaceGoal", "deleteGoal",
+                "appendSubgoal", "getSubgoalByIndex", "replaceSubgoal", "deleteSubgoal"
+            ).map { Tag(it) })
+        }
+    }
 }

21897780db3d7ca0dba5aecce13277fd8f7ff6e8
Alberto Marfoglia <alberto.marfoglia@studio.unibo.it>
2021-12-27 16:56:35 +0100

    Improve lpaas domain

---
+++ domain/src/main/kotlin/it/unibo/lpaas/domain/Functor.kt
@@ -0,0 +1,4 @@
+package it.unibo.lpaas.domain
+
+@JvmInline
+value class Functor(val value: String)
--- domain/src/main/kotlin/it/unibo/lpaas/domain/Theory.kt
+++ domain/src/main/kotlin/it/unibo/lpaas/domain/Theory.kt
@@ -1,7 +1,16 @@
 package it.unibo.lpaas.domain
 
+import it.unibo.tuprolog.core.Clause
 import java.time.Instant
 import it.unibo.tuprolog.theory.Theory as Theory2P
+
+fun Theory2P.getFactsByFunctor(functor: Functor) =
+    clauses
+        .map { println(it); it }
+        .filter(Clause::isFact)
+        .map { it.head }
+        .filter { it?.functor == functor.value }
+        .map { Fact(it.toString()) }
 
 data class Theory(
     val name: TheoryId,

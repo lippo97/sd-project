b8489b924c1d2a33439d3869dc2574796c7aedb6
Alberto Marfoglia <alberto.marfoglia@studio.unibo.it>
2021-12-27 12:35:25 +0100

    Change TheoryRepository interface

--- core/src/main/kotlin/it/unibo/lpaas/core/persistence/TheoryRepository.kt
+++ core/src/main/kotlin/it/unibo/lpaas/core/persistence/TheoryRepository.kt
@@ -5,12 +5,19 @@ import it.unibo.lpaas.core.exception.NotFoundException
 import it.unibo.lpaas.core.exception.ValidationException
 import it.unibo.lpaas.domain.Theory
 import it.unibo.lpaas.domain.TheoryId
+import it.unibo.lpaas.domain.Version
 
 interface TheoryRepository {
     suspend fun findAll(): List<Theory>
 
+    /**
+     * Retrieves the latest version of the theory.
+     */
     @Throws(NotFoundException::class)
     suspend fun findByName(name: TheoryId): Theory
+
+    @Throws(NotFoundException::class)
+    suspend fun findByNameAndVersion(name: TheoryId, version: Version): Theory
 
     @Throws(ValidationException::class, DuplicateIdentifierException::class)
     suspend fun create(name: TheoryId, data: Theory.Data): Theory
@@ -18,6 +25,15 @@ interface TheoryRepository {
     @Throws(ValidationException::class, NotFoundException::class)
     suspend fun updateByName(name: TheoryId, data: Theory.Data): Theory
 
+    /**
+     * Deletes all the versions of the selected theory.
+     */
     @Throws(NotFoundException::class)
-    suspend fun deleteByName(name: TheoryId): Theory
+    suspend fun deleteAllVersionsByName(name: TheoryId): Theory
+
+    /**
+     * Delete the selected version of the selected theory.
+     */
+    @Throws(NotFoundException::class)
+    suspend fun deleteByNameAndVersion(name: TheoryId, version: Version): Theory
 }

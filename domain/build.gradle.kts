7c94bcda087e8f6274795a15eb2da6b7166e264e
Alberto Marfoglia <alberto.marfoglia@studio.unibo.it>
2021-12-27 16:15:34 +0100

    Expose tuprolog theory domain

--- domain/build.gradle.kts
+++ domain/build.gradle.kts
@@ -6,7 +6,7 @@ dependencies {
     implementation(project(":utils"))
 
     api("it.unibo.tuprolog", "core", "0.20.4")
-    implementation("it.unibo.tuprolog", "theory", "0.20.4")
+    api("it.unibo.tuprolog", "theory", "0.20.4")
     implementation("it.unibo.tuprolog", "parser-core", "0.20.4")
     implementation("it.unibo.tuprolog", "parser-theory", "0.20.4")
 }

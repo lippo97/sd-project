475f8d98b5e35e2f8820edf749d77af62adf5381
Alberto Marfoglia <alberto.marfoglia@studio.unibo.it>
2022-02-18 11:46:05 +0100

    Fix credentials serialization on client

--- client/src/main/kotlin/it/unibo/lpaas/client/application/Application.kt
+++ client/src/main/kotlin/it/unibo/lpaas/client/application/Application.kt
@@ -1,8 +1,14 @@
 package it.unibo.lpaas.client.application
 
+import UsernameDeserializer
 import com.github.ajalt.clikt.core.NoOpCliktCommand
 import com.github.ajalt.clikt.core.subcommands
 import io.vertx.core.json.jackson.DatabindCodec
+import it.unibo.lpaas.authentication.provider.Password
+import it.unibo.lpaas.authentication.provider.Username
+import it.unibo.lpaas.authentication.serialization.PasswordDeserializer
+import it.unibo.lpaas.authentication.serialization.PasswordSerializer
+import it.unibo.lpaas.authentication.serialization.UsernameSerializer
 import it.unibo.lpaas.domain.GoalId
 import it.unibo.lpaas.domain.IncrementalVersion
 import it.unibo.lpaas.domain.SolutionId
@@ -17,6 +23,10 @@ fun main(args: Array<String>) {
         addAbstractTypeMapping(GoalId::class.java, StringId::class.java)
         addAbstractTypeMapping(TheoryId::class.java, StringId::class.java)
         addAbstractTypeMapping(SolutionId::class.java, StringId::class.java)
+        addSerializer(Username::class.java, UsernameSerializer())
+        addSerializer(Password::class.java, PasswordSerializer())
+        addDeserializer(Username::class.java, UsernameDeserializer())
+        addDeserializer(Password::class.java, PasswordDeserializer())
     }.applyOn(listOf(DatabindCodec.prettyMapper(), DatabindCodec.mapper()))
 
     NoOpCliktCommand()
